68ea293e18ed4dd756ec9d5747f75449cd5b5b02
Takashi Yamashina <takashi.yamashina@gmail.com>
2026-01-12 18:32:27 +0900

    delete all files

--- benchmarks/main.py
+++
@@ -1,8 +0,0 @@
-from pykour import Pykour
-
-app = Pykour()
-
-
-@app.get("/")
-async def index():
-    return {"message": "Hello, World!"}
--- src/pykour/globals.py
+++
@@ -1,3 +0,0 @@
-import threading
-
-thread_local = threading.local()
